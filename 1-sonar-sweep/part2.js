985bcf44f4b8fc0561665f7e76e4d439b61f31ee
Nicolas 0swin Roger <nicolas@oswin.io>
2021-12-01 19:20:43 +0000

    part2

---
+++ 1-sonar-sweep/part2.js
@@ -0,0 +1,23 @@
+// import input
+const fs = require("fs");
+const input = fs.readFileSync("input.txt", "utf8");
+
+// split the input into an array of strings
+const inputArray = input.split("\n");
+
+// count number of currentMeasurements greater than previousMeasurements
+let previousMeasurement = 0;
+let count = 0;
+
+for (let index = 0; index < inputArray.length; index++) {
+  let currentMeasurement =
+    parseInt(inputArray[index - 1]) +
+    parseInt(inputArray[index]) +
+    parseInt(inputArray[index + 1]);
+  if (currentMeasurement > previousMeasurement) {
+    count++;
+  }
+  previousMeasurement = currentMeasurement;
+}
+
+console.log(count);
