7525f9b6e6213f897cb0beb0483365b6375d9129
Nicolas 0swin Roger <nicolas@oswin.io>
2021-12-02 11:35:40 +0000

    Day 2 Part 1

---
+++ 2-dive/part1.js
@@ -0,0 +1,27 @@
+// import input
+const fs = require('fs');
+const input = fs.readFileSync('input.txt', 'utf8');
+
+// split the input into an array of strings
+const inputArray = input.split('\n');
+
+let horizontal = 0;
+let depth = 0;
+
+for (let index = 0; index < inputArray.length; index++) {
+  instruction = inputArray[index].split(' ');
+  // console.log(instruction);
+  if (instruction[0] === 'up') {
+    depth -= parseInt(instruction[1]);
+  } else if (instruction[0] === 'down') {
+    depth += parseInt(instruction[1]);
+  } else if (instruction[0] === 'forward') {
+    horizontal += parseInt(instruction[1]);
+  }
+}
+
+console.log('Depth: ' + depth);
+console.log('Horizontal: ' + horizontal);
+
+let result = depth * horizontal;
+console.log('Result: ' + result);
