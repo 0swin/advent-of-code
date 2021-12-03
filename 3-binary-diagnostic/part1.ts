ffaa878d3ccbfd57f79ca448731ad8fe696b0437
Nicolas "0swin" Roger <nicolas@oswin.io>
2021-12-03 08:57:32 +0100

    day 3 part 1

---
+++ 3-binary-diagnostic/part1.ts
@@ -0,0 +1,48 @@
+// import input
+import fs from 'fs';
+const input = fs.readFileSync('input.txt', 'utf8');
+
+// split the input into an array of strings
+const inputArray = input.split('\n');
+
+// define average as array with inputArray.lenght items set to 0
+let gamma = new Array(inputArray[0].length).fill(0);
+let epsilon = new Array(inputArray[0].length).fill(0);
+
+// for each string in the array
+for (let i = 0; i < inputArray.length; i++) {
+  // split into numbers
+  const inputNumbers = inputArray[i].split('');
+  
+  // for each number in the array
+  for (let j = 0; j < inputNumbers.length; j++) {
+    // add the number to the average
+    gamma[j] += parseInt(inputNumbers[j]);
+  }
+}
+
+// for each number in the average
+for (let i = 0; i < gamma.length; i++) {
+  // if the number is superior to half of inputArray.length then 1 else 0
+  if (gamma[i] > inputArray.length / 2) {
+    gamma[i] = 1;
+    epsilon[i] = 0;
+  } else {
+    gamma[i] = 0;
+    epsilon[i] = 1;
+  }
+}
+
+// convert average to number
+const gammaBinary = gamma.join('');
+console.log(gammaBinary)
+const epsilonBinary = epsilon.join('');
+console.log(epsilonBinary)
+
+// convert averageBinary from binary to decimal
+const gammaDecimal = parseInt(gammaBinary, 2);
+console.log(gammaDecimal);
+const epsilonDecimal = parseInt(epsilonBinary, 2);
+console.log(epsilonDecimal);
+
+console.log(epsilonDecimal * gammaDecimal);
