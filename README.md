7be362e606f1479e30824a42052c17283f3ff1d0
Nicolas 0swin Roger <nicolas@oswin.io>
2021-12-07 19:25:02 +0000

    day 7 part 2

--- README.md
+++ README.md
@@ -17,7 +17,7 @@
 [![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%86/yellow)](src/day04)
 [![Day](https://badgen.net/badge/05/%E2%98%85%E2%98%86/yellow)](src/day05)
 [![Day](https://badgen.net/badge/06/%E2%98%85%E2%98%86/yellow)](src/day06)
-[![Day](https://badgen.net/badge/07/%E2%98%85%E2%98%86/yellow)](src/day07)
+[![Day](https://badgen.net/badge/07/%E2%98%85%E2%98%85/green)](src/day07)
 ![Day](https://badgen.net/badge/08/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/09/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/10/%E2%98%86%E2%98%86/gray)
@@ -111,9 +111,9 @@ Both parts: 48.792834ms
 
 ```
 Day 07
-Time part 1: 0.74ms
-Time part 2: -
-Both parts: 0.739509ms
+Time part 1: 0.863ms
+Time part 2: 48.96ms
+Both parts: 49.822541ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 9/50
-Total time: 92.078ms
+Total stars: 10/50
+Total time: 141.161ms
 ```
 
 <!--/RESULTS-->
--- src/day07/index.ts
+++ src/day07/index.ts
@@ -9,15 +9,27 @@ const part1 = (rawInput: string) => {
     .sort((a, b) => (a > b ? 1 : -1));
   let medianPos = input[input.length / 2];
   let fuel = input.map((x) => Math.abs(x - medianPos));
-  let totalFuel = fuel.reduce((a, b) => a + b);
+  let totalFuel = fuel.reduce((a, b) => a + b, 0);
 
   return `${totalFuel}`;
 };
 
 const part2 = (rawInput: string) => {
-  const input = parseInput(rawInput);
+  const input = parseInput(rawInput)
+    .split(',')
+    .map((x) => parseInt(x))
+    .sort((a, b) => (a > b ? 1 : -1));
 
-  return;
+  let leastFuel = Infinity;
+  for (let i = 0; i < input.length; i++) {
+    let moves = input.map((x) => Math.abs(x - i));
+    let fuel = moves.map((x) => (x * (x + 1)) / 2);
+    let totalFuel = fuel.reduce((a, b) => a + b, 0);
+    if (totalFuel < leastFuel) leastFuel = totalFuel;
+  }
+  console.log(leastFuel);
+
+  return `${leastFuel}`;
 };
 
 run({
@@ -32,10 +44,10 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `16,1,2,0,4,2,7,1,2,14`,
+        expected: '168',
+      },
     ],
     solution: part2,
   },
