3cacfbbc404d30634399c759e7ffe8891ff9feb8
Nicolas 0swin Roger <nicolas@oswin.io>
2021-12-08 12:26:50 +0000

    day 8 part 1

--- README.md
+++ README.md
@@ -18,7 +18,7 @@
 [![Day](https://badgen.net/badge/05/%E2%98%85%E2%98%86/yellow)](src/day05)
 [![Day](https://badgen.net/badge/06/%E2%98%85%E2%98%85/green)](src/day06)
 [![Day](https://badgen.net/badge/07/%E2%98%85%E2%98%85/green)](src/day07)
-![Day](https://badgen.net/badge/08/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/08/%E2%98%85%E2%98%86/yellow)](src/day08)
 ![Day](https://badgen.net/badge/09/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/10/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/11/%E2%98%86%E2%98%86/gray)
@@ -104,9 +104,9 @@ Both parts: 78.705877ms
 
 ```
 Day 06
-Time part 1: 115.939ms
-Time part 2: 0.553ms
-Both parts: 116.492784ms
+Time part 1: 0.521ms
+Time part 2: 0.26ms
+Both parts: 0.781115ms
 ```
 
 ```
@@ -118,9 +118,9 @@ Both parts: 48.844888ms
 
 ```
 Day 08
-Time part 1: -
+Time part 1: 2.742ms
 Time part 2: -
-Both parts: -
+Both parts: 2.741732ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 11/50
-Total time: 271.666ms
+Total stars: 12/50
+Total time: 158.697ms
 ```
 
 <!--/RESULTS-->
---
+++ src/day08/index.ts
@@ -0,0 +1,65 @@
+import run from 'aocrunner';
+
+const parseInput = (rawInput: string) => rawInput;
+
+const part1 = (rawInput: string) => {
+  let input = parseInput(rawInput)
+    .split('\n')
+    .map((line) => line.split(' | ')[1].split(' '));
+
+  // .forEach((line) => line.split(' | ')[1]);
+  console.log(input);
+
+  let length = Array(8).fill(0);
+  input.forEach((line) => {
+    line.forEach((word) => {
+      length[word.length - 1]++;
+    });
+  });
+
+  console.log(length);
+
+  return `${length[1] + length[2] + length[3] + length[6]}`;
+};
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  return;
+};
+
+const testInput = `
+  be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+  edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+  fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+  fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+  aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+  fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+  dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+  bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+  egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+  gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
+`;
+
+run({
+  part1: {
+    tests: [
+      {
+        input: testInput,
+        expected: '26',
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: true,
+});
